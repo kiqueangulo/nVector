b42e45e6d12b4199aadb6444b4f8151cb5a44e70
kiqueangulo <kiqueangulo@gmail.com>
2022-07-18 10:28:56 -0400

    Basic horizontal scrollbar created

---
+++ src/components/ScrollBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import './ScrollBar.css';
+
+function ScrollBar() {
+    return (
+        <div className="section-wrapper">
+            <div className="section-item">Section 1</div>
+            <div className="section-item">Section 2</div>
+            <div className="section-item">Section 3</div>
+            <div className="section-item">Section 4</div>
+            <div className="section-item">Section 5</div>
+            <div className="section-item">Section 5</div>
+            <div className="section-item">Section 7</div>
+            <div className="section-item">Section 1</div>
+            <div className="section-item">Section 2</div>
+            <div className="section-item">Section 3</div>
+            <div className="section-item">Section 4</div>
+            <div className="section-item">Section 5</div>
+            <div className="section-item">Section 5</div>
+            <div className="section-item">Section 7</div>
+        </div>
+    );
+};
+
+export default ScrollBar;
