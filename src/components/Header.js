e1856aa654f05dbcaab075803560641799050332
kiqueangulo <kiqueangulo@gmail.com>
2022-07-19 14:14:03 -0400

    Dropdown menu created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from "./Navbar"
+import Navbar from "./Navbar/Navbar.js";
 import SaveButton from "./SaveButton"
 
 import Logo from "./Logo"
@@ -12,15 +13,27 @@ function Header() {
         flexflow: 'row nowrap',
         justifyContent: 'space-between',
         alignItems: 'center',
-        padding: '1px 3px',
+        padding: '1px 5px',
+        marginTop: '5px',
         marginBottom: '5px'
     };
     
 	return (
 		<div style={headerStyle}>
+            <Router>
                 <Navbar />
-                <Logo />
-                <SaveButton />
+                <Routes>
+                    <Route path="/user" />
+                    <Route path="/" />
+                    <Route path="/new" />
+                    <Route path="/locations" />
+                    <Route path="/synch" />
+                    <Route path="/support" />
+                    <Route path="/log" />
+                </Routes>
+            </Router>
+            <Logo />
+            <SaveButton />
 		</div>
 	)
 }
---
+++ src/components/Navbar/SidebarData.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+    {
+        title: 'User Name',
+        path: '/user',
+        icon: <FaIcons.FaUserAlt />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Add Location',
+        path: '/new',
+        icon: <AiIcons.AiFillFileAdd />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Search',
+        path: '/locations',
+        icon: <FaIcons.FaWarehouse />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Sync',
+        path: '/sync',
+        icon: <FaIcons.FaSyncAlt />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Support',
+        path: '/support',
+        icon: <IoIcons.IoMdHelpCircle />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Log Out',
+        path: '/log',
+        icon: <AiIcons.AiOutlineLogout />,
+        cName: 'nav-text'
+    }
+]
