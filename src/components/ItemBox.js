7ef23dce9136ea842e85011d44dc258d6ddc143e
AJ Phoenix <ajph03nix@gmail.com>
2022-07-23 10:41:14 -0400

    functioning/explanding sections/clickablility

--- src/components/ItemBox.js
+++ src/components/ItemBox.js
@@ -2,33 +2,37 @@ import React,{useState} from "react";
 
 import './ItemBox.css';
 
+
 function ItemBox(props) {
     let items = props.items
-    let className = "itemBox"
 
-    const [count,setCount] = useState(0)
+    const [count,setCount] = useState(200)
     //Displays an array of itmes from a call to the  
     const displayItems = items.map((item, index) => (
         
         <li key={index}>
-            <div  className={className}>
-                {/* <div className="itemName"> */}
+             
+            <div  className="itemBox">
+                {/* <div className="itemQuantBox"> */}
+                <button className="infoButton">
+                        i
+                </button>
                 <textarea class="scrollableTextBox" name="itemName">
                     {item}
                 </textarea>
-                     
-                {/* </div> */}
+                        
                 <div className="quantField">
-                   <button className="minusButton" 
-                         onClick={() =>{ if (count > 0){setCount(count - 1)}else{count=0} }}> - 
+                <button className="minusButton" 
+                        onClick={() =>{ if (count > 0){setCount(count - 1)}else{count=0} }}> - 
                     </button> 
-                   {/* <input value={item.qauntity} maxLength="5"></input> */}
-                   <textbox class="quantTextBox" name="itemQnt">
-                        {count} {/* Just to have a number */}
+                {/* <input value={item.qauntity} maxLength="5"></input> */}
+                <textbox class="quantTextBox" name="itemQnt">
+                        {count}       {/* Just to have a number */}
                     </textbox>
                     <button className="plusButton" 
                             onClick={() => setCount(count + 1)}> + </button>
                 </div>
+                {/* </div> */}
                 <div className="remove">
                    remove
                 </div>
--- src/components/SubBox.js
+++ src/components/SubBox.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React,{useState} from "react";
+import Collapsible from 'react-collapsible';
 import ItemBox from "./ItemBox";
 
 import './SubBox.css';
@@ -10,23 +11,35 @@ function genTestItems(amt){
     }
     return testItems
 }
+//Sections have array of items assigned to them
 
-
+//Generate subsection tab
+function subSectionTab(index){
+    return(
+        <button type="button" class="collapsibleButton">
+                Shelf {index} ------------
+            </button>
+    ) 
+    
+}
 function SubBox(props) {
+    // const [sectTab, setSectTab] = useState({});
     
     const displaySubSections = props.subSections.map((subsection, index) => (
         // className = subsection + content
         <li key={index}>
-            <button type="button" class="collapsible">
-                Sub-Section: {index} ______________
-            </button>
-                <div className={subsection}>
+            <div className="line">
+
+            </div>
+              <Collapsible open trigger= {subSectionTab(index)} lazyRender= 'false' className="">
+                <div className="content">
+                    {/* // {subsection}> */}
                     <ItemBox items={genTestItems(5)} />
                 </div>
+                </Collapsible>
         </li>
     ));
 	return (
-
 		<div className ="subBox">
             {displaySubSections}
 		</div>
