a44e5549b5c47209eb33a2f39ab37676d33f2d9a
AJ Phoenix <ajph03nix@gmail.com>
2022-07-25 12:46:46 -0400

    Styled NavBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -33,7 +33,7 @@ function Navbar() {
 
   return (
     <>
-      <IconContext.Provider value={{ color: "#000" }}>
+      <IconContext.Provider value={{ color: "#868686" }}>
         <div className="navbar">
           <Link to="#" className="menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
