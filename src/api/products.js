b24815efe083a85a8a72fe7d304b435a5784c04f
kiqueangulo <kiqueangulo@gmail.com>
2022-07-25 08:41:21 -0400

    Console errors fixed and product calls created

--- src/api/products.js
+++ src/api/products.js
@@ -4,5 +4,15 @@ const PRODUCT_URL =
   "https://nventory-postgres-database.herokuapp.com/products/";
 
 const getAllProducts = () => axios.get(PRODUCT_URL);
+const createProduct = (body) => axios.post(PRODUCT_URL, body);
+const getOneProduct = (id) => axios.get(`${PRODUCT_URL}/${id}`);
+const updateProduct = (id) => axios.put(`${PRODUCT_URL}/${id}`);
+const deleteProduct = (id) => axios.delete(`${PRODUCT_URL}/${id}`);
 
-export const productsCall = { getAllProducts };
+export const productsCall = {
+  getAllProducts,
+  createProduct,
+  getOneProduct,
+  updateProduct,
+  deleteProduct,
+};
