688ea99811da2a8d1b1d13200ef289e1f792bf16
AJ Phoenix <ajph03nix@gmail.com>
2022-08-01 10:11:10 -0400

    Section name now displays at top of section

--- src/components/SectionBox.js
+++ src/components/SectionBox.js
@@ -5,8 +5,15 @@ import SubBox from "./SubBox";
 import "./SectionBox.css";
 
 function SectionBox(props) {
+    const url = new URL(window.location);
+    const urlPath = url.pathname.slice(1);
+    console.log(urlPath)
+    // const sectionName = 
   return (
     <div className="sectionBody">
+      <div className="sectionHeader">
+       <h2> {urlPath}</h2>
+      </div>
       <SubBox subSections={props.subSections} />
     </div>
   );
